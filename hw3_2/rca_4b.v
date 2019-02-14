96e97e39ed963dcab95ae9fc23b768a4bdad1b9e
Amanda <becker23@wisc.edu>
2019-02-14 14:11:48 -0600

    rca_4b compiles, tb doesn't

--- hw3_2/rca_4b.v
+++ hw3_2/rca_4b.v
@@ -1,7 +1,7 @@
 /*
     CS/ECE 552 Spring '19
     Homework #3, Problem 2
-    
+
     a 4-bit RCA module
 */
 module rca_4b(A, B, C_in, S, C_out);
@@ -15,5 +15,13 @@ module rca_4b(A, B, C_in, S, C_out);
     output         C_out;
 
     // YOUR CODE HERE
+    wire C0;
+    wire C1;
+    wire C2;
+
+    fullAdder_1b fa0(.A(A[0]), .B(B[0]), .C_in(C_in), .S(S[0]), .C_out(C0));
+    fullAdder_1b fa1(.A(A[1]), .B(B[1]), .C_in(C0), .S(S[1]), .C_out(C1));
+    fullAdder_1b fa2(.A(A[2]), .B(B[2]), .C_in(C1), .S(S[2]), .C_out(C2));
+    fullAdder_1b fa3(.A(A[3]), .B(B[3]), .C_in(C2), .S(S[3]), .C_out(C_out));
 
 endmodule
---
+++ hw3_2/rca_4b_tb.v
@@ -0,0 +1,51 @@
+module rca_4b_tb;
+
+  reg [3: 0] A, B;
+  reg C_in;
+  wire [3:0] S;
+  wire C_out;
+
+  wire Clk;
+  //2 dummy wires
+  wire rst;
+  wire err;
+
+  //clkrst module instantiation
+  clkrst my_clkrst( .clk(Clk), .rst(rst), .err(err));
+
+  //full adder module instantiation
+  rca_4b DUT (.A(A), .B(B), .C_in(C_in), .S(S), .C_out(C_out));
+
+  initial begin
+    A = 0;
+    B = 0;
+    C_in = 0;
+
+    #200;
+  end
+
+  always@(posedge Clk) begin
+    #10;
+    if (S != 0 || C_out != 0) $display("ERROR 0!");
+
+    A = 0x8;
+    B = 0x2;
+    C_in = 0;
+    #10;
+    if (S != 0xA || C_out != 0) $display("ERROR 1!");
+
+    A = 0xF;
+    B = 0x1;
+    C_in = 0;
+    #10;
+    if (S != 0x0 || C_out != 1) $display("ERROR 2!");
+
+    A = 0xA;
+    B = 0x5;
+    C_in = 1;
+    #10;
+    if (S != 0x0 || C_out != 1) $display("ERROR 3!");
+
+    $stop;
+  end
+endmodule
